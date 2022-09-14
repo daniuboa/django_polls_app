098bfabc7144440b555d228b37861232ef9e77ed
Dani Uboa <uboadaniel@gmail.com>
2022-09-14 17:16:19 +0300

    update views.py

--- poll/views.py
+++ poll/views.py
@@ -1,3 +1,4 @@
+from http.client import HTTPResponse
 from multiprocessing import context
 from django.shortcuts import render, redirect
 from .forms import CreatePollForm
@@ -8,7 +9,10 @@ from .models import Poll
 def home(request):
     polls = Poll.objects.all()
     
-    context = {'polls': polls}
+    context = {
+        'polls': polls
+    }
+    
     return render(request, 'poll/home.html', context)
 
 def create(request):
@@ -16,22 +20,42 @@ def create(request):
         form = CreatePollForm(request.POST)
         if form.is_valid():
             form.save()
+            return redirect('home')
     else:
         form = CreatePollForm()
         
-    context = {'form': form}
+    context = {
+        'form': form
+    }
     
-    return redirect('home')
+    return render(request, 'poll/create.html', context)
 
 def results(request, poll_id):
-    context ={}
+    poll = Poll.objects.get(pk=poll_id)
+    
+    context ={
+        'poll': poll
+    }
     return render(request, 'poll/results.html', context)
 
 def vote(request, poll_id):
     poll = Poll.objects.get(pk=poll_id)
     
     if request.method == 'POST':
-        print(request.POST['poll'])
+        
+        select_option = request.POST['poll']
+        if select_option == 'option1':
+            poll.option_one_count += 1
+        elif select_option == 'option2':
+            poll.option_two_count += 1
+        elif select_option == 'option3':
+            poll.option_three_count += 1
+        else:
+            return HTTPResponse(400, 'Invalid form')
+        
+        poll.save()
+        
+        return redirect('results', poll.id)
           
     context ={
         'poll': poll
