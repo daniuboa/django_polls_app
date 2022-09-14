c85901e5c1174100682e0369c5abec842e5c8d06
Dani Uboa <uboadaniel@gmail.com>
2022-09-14 14:22:02 +0300

    update vies.py

--- poll/views.py
+++ poll/views.py
@@ -3,13 +3,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'poll/home.html', context)
+    return render(request, 'poll/home.html', context={})
 
 def create(request):
-    return render(request, 'poll/create.html', context)
+    return render(request, 'poll/create.html', context={})
 
 def results(request):
-    return render(request, 'poll/results.html', context)
+    return render(request, 'poll/results.html', context={})
 
 def vote(request):
-    return render(request, 'poll/vote.html', context)
+    return render(request, 'poll/vote.html', context={})
